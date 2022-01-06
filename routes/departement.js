2ad553b2e0a84059511a9e795cee43a1aced6000
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-01-06 11:16:46 +0100

    findAll departement

--- routes/departement.js
+++ routes/departement.js
@@ -1,67 +1,53 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../config/dbConfig');
-const Gig = require('../models/departementModel');
-const Sequelize = require('sequelize');
-const { Departement } = require('../models');
-const departement = require('../models/departementModel');
-const Op = Sequelize.Op;
+/*                              departement Route                           */
+/* -------------------------------------------------------------------------- */
 
-// Get depar list
-router.get('/', (req, res) => 
-  Departement.findAll()
-    .then(departement => res.render('departement', {
-        departement
-      }))
-    .catch(err => res.render('error', {error: err})));
+const express = require('express')
+const router = express.Router()
+const { Departement } = require('../models')
 
-// Add a depar
-router.post('/add', (req, res) => {
-  let { name, description } = req.body;
-  let errors = [];
-
-  // Validate Inputs
-  if(!name) {
-    errors.push({ text: 'Please add a name' });
-  }
-  if(!description) {
-    errors.push({ text: 'Please add a description' });
-  }
-
-  // Check for errors
-  if(errors.length > 0) {
-    res.render('add', {
-      errors,
-      name, 
-      description
-    });
-  } else {
-    if(!budget) {
-      budget = 'Unknown';
-    } else {
-      budget = `$${budget}`;
-    }
-
-    // Insert into table
-    Departement.create({
-      name,
-      description
+router
+    .route('/departement')
+    .get((rep, res) => {
+        Departement.findAll().then(dep => { // les param lighdi i findi liya ghadi i t stockaw f 'dep'
+            res.render('depar/home', {
+                'departements' : dep
+            });
+        })
     })
-      .then(departement => res.redirect('/departement'))
-      .catch(err => res.render('error', {error:err.message}))
-  }
-});
 
-// Search for gigs
-router.get('/search', (req, res) => {
-  let { term } = req.query;
+router
+    .route('/departement/create')
+    .get((rep, res) => {
+        res.render('depar/create')
+    })
 
-  // Make lowercase
-  term = term.toLowerCase();
+    // Departement.create(rep.body).then(dep => {
+    //     res.render('depar/create')
+    // })
 
-  Gig.findAll({ where: { technologies: { [Op.like]: '%' + term + '%' } } })
-    .then(gigs => res.render('gigs', { gigs }))
-    .catch(err => res.render('error', {error: err}));
-});
+router
+    .route('/departement/edit/:id')
+    .get((rep, res) => {
+        Departement.findAll({
+            where: { id: rep.params.id }
+        }).then(dep => {
+            res.json(dep)
+        })
+    })
+    .post((rep, res) => {
 
-module.exports = router;
+        Departement.findOne({ where: { id: rep.params.id } })
+            .then(function (project) {
+                project.update({
+                    name: rep.body.name
+                })
+                .then(function (project) {
+                    res.json(project)
+                })
+            })
+            .catch((e) => console.log(e.message))
+    })
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,36 +1,25 @@
 // const path = require('path');
 const express = require('express');
 const ejs = require('ejs');
-// const bodyParser = require('body-parser');
-// const mysql = require('mysql2');
-const cors = require('cors');
-
-const chihaja = require('./models/index');
+const models = require('./models/index');
 
 const app = express();
-
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-const db = require('./config/dbConfig');
-// Test DB
-// db.authenticate()
-//   .then(() => console.log('Database connected...'))
-//   .catch(err => console.log('Error: ' + err))
-
+app.set('view engine', 'ejs');
 
 // MIddleware
-app.use(cors(corsOptions));
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // For handler parame of post req sous form json ( string => json)
+app.use(express.urlencoded({ extended: true })); // For handler parame of post req
 
 //Testiiiiiiiiiiiiing
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Hell!!!." });
 });
+
+//* Require Routes
+const deparRoute = require('./routes/departement');
+
+//* Register Our Routes
+app.use('/',deparRoute)
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
